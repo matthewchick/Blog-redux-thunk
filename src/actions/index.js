04c5a47231108558bab6e45e458d5b3408a28e25
Matthew Chick <c.chick@unsw.edu.au>
2019-10-25 10:24:28 +1100

    Error: Actions must be plain object. Use custom middleware for async actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,13 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
 //Action creator
-export const fetchPosts =() => {
+export const fetchPosts = async() => {
+
+    // bad approach: Actions must be plain object. Use custom middleware for async actions
+    const response = await jsonPlaceholder.get('/posts');
+    console.log(response);
+
     return {
-        type: 'FETCH_POSTS'
+        type: 'FETCH_POSTS',
+        payload: response
     };
 };
